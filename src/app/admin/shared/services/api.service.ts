bb4e0bfd2fab413a26db34c988b74623aca0f38b
Roma <roman_biliak@i.ua>
2020-04-26 22:39:01 +0300

    api

--- src/app/admin/shared/services/api.service.ts
+++ src/app/admin/shared/services/api.service.ts
@@ -4,7 +4,8 @@ import {LoginUser, Token} from '../interfaces';
 import {Observable} from 'rxjs';
 import { TokenService } from './token.service';
 import { Image } from '../../image/image';
-
+import { User } from '../../user/user';
+import { Filter } from '../../filter/filter';
 @Injectable()
 export class ApiService {
 
@@ -25,6 +26,71 @@ export class ApiService {
   public getImage(id: number): Observable<any> {
     const headers = this.Headers;
     return this.http.get(this.BASE_URL + 'image/' + id, {headers});
+  }
+
+  public createImage(image: Image): Observable<any> {
+    const headers = this.Headers;
+    return this.http.post(this.BASE_URL + 'image/', image, {headers});
+  }
+
+  public updateImage(image: Image): Observable<any> {
+    const headers = this.Headers;
+    return this.http.put(this.BASE_URL + 'image/', image, {headers});
+  }
+
+  public deleteImage(id: number): Observable<any>{
+    const headers = this.Headers;
+    return this.http.delete(this.BASE_URL + 'image/' + id, {headers});
+  }
+
+  public getFilters(): Observable<any>{
+    const headers = this.Headers;
+    return this.http.get(this.BASE_URL + 'filters', {headers});
+  }
+
+  public getFilter(id: number): Observable<any> {
+    const headers = this.Headers;
+    return this.http.get(this.BASE_URL + 'filter/' + id, {headers});
+  }
+
+  public createFilter(filter: Filter): Observable<any> {
+    const headers = this.Headers;
+    return this.http.post(this.BASE_URL + 'filter/', filter, {headers});
+  }
+
+  public updateFilter(filter: Filter): Observable<any> {
+    const headers = this.Headers;
+    return this.http.put(this.BASE_URL + 'filter/', filter, {headers});
+  }
+
+  public deleteFilter(id: number): Observable<any>{
+    const headers = this.Headers;
+    return this.http.delete(this.BASE_URL + 'flter/' + id, {headers});
+  }
+
+  public getUsers(): Observable<any>{
+    const headers = this.Headers;
+    return this.http.get(this.BASE_URL + 'users', {headers});
+  }
+
+  public getUser(id: number): Observable<any> {
+    const headers = this.Headers;
+    return this.http.get(this.BASE_URL + 'user/' + id, {headers});
+  }
+
+  public createUser(user: User): Observable<any> {
+    const headers = this.Headers;
+    return this.http.post(this.BASE_URL + 'image/', user, {headers});
+  }
+
+  public updateUser(user: User): Observable<any> {
+    const headers = this.Headers;
+    return this.http.put(this.BASE_URL + 'image/', user, {headers});
+  }
+
+  public deleteUser(id: number): Observable<any>{
+    const headers = this.Headers;
+    return this.http.delete(this.BASE_URL + 'user/' + id, {headers});
   }
 
   public get Headers(): HttpHeaders{
