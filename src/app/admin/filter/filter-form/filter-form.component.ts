7606f295579044953df24cf70b159e31323ef8f1
Roma <roman_biliak@i.ua>
2020-04-27 23:33:17 +0300

    filter component

--- src/app/admin/filter/filter-form/filter-form.component.ts
+++ src/app/admin/filter/filter-form/filter-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Filter} from '../../filter/filter';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {FilterService} from '../../filter/filter.service';
 
 @Component({
   selector: 'app-filter-form',
@@ -7,9 +11,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterFormComponent implements OnInit {
 
-  constructor() { }
+  public errorMessage: string;
+  public filterData: Filter;
+  public filterForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private filter: FilterService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm();
+    this.getFilter();
+  }
+
+  private getFilter() {
+    this.activatedRoute.params.forEach((params: Params) => {
+      const id = params.id;
+      if (id) {
+        this.filter.getFilter(id).subscribe(
+          filter => {
+            this.filterData = filter;
+            this.filterForm.patchValue(this.filterData);
+          },
+          error => this.errorMessage = error
+        );
+      }
+      else {
+        this.filterData = new Filter({name: '', order: 0, status: 0});
+        this.filterForm.patchValue(this.filterData);
+      }
+    });
+  }
+
+  public onSubmit(filterForm: FormGroup) {
+    this.filterData.name = filterForm.value.name;
+    this.filterData.order = parseInt(filterForm.value.order);
+    this.filterData.status = filterForm.value.status ? 1 : 0;
+
+    if (this.filterData.id) {
+      this.filter.updateFilter(this.filterData)
+        .subscribe(
+          () => this.goBack(),
+          error => this.errorMessage = error
+        );
+    } else {
+      this.filter.addFilter(this.filterData)
+        .subscribe(
+          () => this.goBack(),
+          error => this.errorMessage = error
+        );
+    }
+  }
+  private goBack() {
+    this.router.navigate(['/filters'], {relativeTo: this.activatedRoute});
+  }
+
+  public checkError(element: string, errorType: string) {
+    return this.filterForm.get(element).hasError(errorType) &&
+      this.filterForm.get(element).touched;
+  }
+
+  private buildForm() {
+    this.filterForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      order: [0, Validators.required],
+      status: [0]
+    });
   }
 
 }
--- src/app/admin/filter/filter.service.ts
+++ src/app/admin/filter/filter.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@angular/core';
 import {ApiService} from '../shared/services/api.service';
 import {Filter} from './filter';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {HttpErrorResponse} from '@angular/common/http';
+import {AuthService} from '../shared/services/auth.service';
 
 @Injectable()
 export class FilterService {
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private auth: AuthService) { }
 
   public getFilters(): Observable<Filter[]>{
     return this.api.getFilters().pipe(
-      map(this.extractFilters)
+      map(this.extractFilters),
+      catchError(this.handleError.bind(this))
+    );
+  }
+  public getFilter(id: number): Observable<Filter>{
+    return this.api.getFilter(id).pipe(
+      map((filter) => {
+        return new Filter(filter);
+      }),
+      catchError(this.handleError.bind(this))
+    );
+  }
+  public addFilter(filter: Filter): Observable<void>{
+    return this.api.createFilter(filter).pipe(
+      catchError(this.handleError.bind(this))
+    );
+  }
+  public updateFilter(filter: Filter): Observable<void>{
+    return this.api.updateFilter(filter).pipe(
+      catchError(this.handleError.bind(this))
     );
   }
   public deleteFilter(id: number): Observable<any>{
-    return this.api.deleteFilter(id).pipe();
+    return this.api.deleteFilter(id).pipe(
+      catchError(this.handleError.bind(this))
+    );
   }
   private extractFilters(response: any) {
     const filters: Filter[] = [];
@@ -23,5 +46,16 @@ export class FilterService {
       filters.push(new Filter(response[i]));
     }
     return filters;
+  }
+  private handleError(error: HttpErrorResponse){
+    switch (error.status) {
+      case 401:
+        this.auth.logout();
+        break;
+      default:
+        console.dir(error);
+        break;
+    }
+    return throwError(error);
   }
 }
